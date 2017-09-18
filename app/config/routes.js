c5203bc8435514e9a6b7cfe6f6283b4cdd812a30
jazmin10 <jazminestrada10@gmail.com>
2017-09-18 13:30:00 -0500

    adding dummy profile route to routes.js

--- app/config/routes.js
+++ app/config/routes.js
@@ -15,7 +15,7 @@ import router, {Route, Router, browserHistory, IndexRoute} from "react-router";
 import Main from '../components/Main.js';
 // import Splash from '../components/Splash.js';
 import Dashboard from '../components/Dashboard.js';
-// import Profile from '../components/Profile.js';
+import Profile from '../components/Profile.js';
 // import Projects from '../components/Projects.js';
 // import Masterlist from '../components/Masterlist.js';
 // import Add from '../components/Add.js';
@@ -28,15 +28,16 @@ export default (
 
    <Route path="/" component={Main}>
 
-       <Route path="/dashboard-manager" component={Dashboard}>
+       <Route path="dashboard-manager" component={Dashboard}>
         {/*
           <Route path="masterlist" component={Masterlist} />
           <Route path="my-projects" component={Projects} />
-          <Route path="add-car" component={Add} />
+          <Route path="add-car" component={Add} /> 
 
-          <Route path="profile/:vin" component={Profile} />
+          <Route path="profile/:vin" component={Profile} /> */}
+          <Route path="profile/5YFBURHE9EP015823" component={Profile} />
 
-          <IndexRoute component={Projects} /> 
+         {/* <IndexRoute component={Projects} /> 
         */}
        </Route>
 
