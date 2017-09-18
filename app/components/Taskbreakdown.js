a8a7a5bcfa43ec359af502b0b66445638ade3248
Flynn <flynnigan.tan@gmail.com>
2017-09-18 18:07:13 -0500

    capturing checked and delete values in taskbreakdown component

--- app/components/Taskbreakdown.js
+++ app/components/Taskbreakdown.js
@@ -34,6 +34,9 @@ export default class Tasks extends React.Component {
 			categoryName:"Cosmetics",
 			categoryProgress:.5,
 		}
+
+		this.handleCheck = this.handleCheck.bind(this);
+		this.handleDeleteTask = this.handleDeleteTask.bind(this);
 	}
 
 	// handleAddTask(){
@@ -44,16 +47,25 @@ export default class Tasks extends React.Component {
 	// 	// method that handles any changes/user inputs
 	// }
 
-	// handleDeleteTask(){
-	// 	// method that removes tasks
+	handleDeleteTask(event){
+		// method that removes tasks
 
-	// 	// capture the task to be removed
-	// 	// remove that task from maintenance -> category
-	// }
+		// capture the task to be removed
+		var deleteTask = event.target.value;
+		console.log(deleteTask);
 
-	// handleCheck(){
-	// 	// method that handles checked/unchecked boxes
-	// }
+		// remove that task from maintenance -> category
+	}
+
+	handleCheck(event){
+		// method that handles checked/unchecked boxes
+
+		// capture the task that has been checked
+		var checkedTask = event.target.value;
+		console.log(checkedTask);
+
+		// set the check's completed status to the opposite
+	}
 
 	render(){
 
@@ -75,7 +87,7 @@ export default class Tasks extends React.Component {
 						<h3>Task Breakdown</h3>
 
 						<div className="progress">
-							<div className="progress-bar" role="progressbar" style={divStyle} aria-valuenow={((this.state.categoryProgress) * 100) + "%"} aria-valuemin="0" aria-valuemax="100">{((this.state.categoryProgress) * 100) + "%"}</div>
+							<div className="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" style={divStyle} aria-valuenow={((this.state.categoryProgress) * 100) + "%"} aria-valuemin="0" aria-valuemax="100">{((this.state.categoryProgress) * 100) + "%"}</div>
 						</div>
 
 						{
@@ -83,8 +95,8 @@ export default class Tasks extends React.Component {
 							this.state.taskInfo.map((tasks,i)=>{
 								return(
 									<div key={i} className="well">
-										<h3><input onClick={this.handleCheck} type="checkbox" name="finished" autoComplete="off" /> {tasks.name}</h3> 
-										<button className="btn btn-xs btn-danger" onClick={this.handleDeleteTask}>Delete Task</button>
+										<h3><input onClick={this.handleCheck} type="checkbox" name="finished" autoComplete="off" value={tasks.name}/> {tasks.name}</h3> 
+										<button className="btn btn-xs btn-danger" onClick={this.handleDeleteTask} value={tasks.name}>Delete Task</button>
 									</div>
 								);
 							})
