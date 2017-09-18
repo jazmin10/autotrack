71e84ec98b8216e03158b6b3bfec058710043177
Flynn <flynnigan.tan@gmail.com>
2017-09-18 14:16:27 -0500

    updated static file connection

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -11,65 +11,65 @@ import React from 'react';
 import helpers from "./utils/helpers.js";
 
 // include children
-import Information from "./Information.js";
-import MainTasks from "./Maintasks.js";
-import Tasks from "./Taskbreakdown.js";
+// import Information from "./Information.js";
+// import MainTasks from "./Maintasks.js";
+// import Tasks from "./Taskbreakdown.js";
 
 // PROFILE -----------------------------
 
 // create and export the profile component
 export default class Profile extends React.Component {
 	
-	// Initial state setup
-	constructor(props){
-		super(props);
+	// // Initial state setup
+	// constructor(props){
+	// 	super(props);
 
-		this.state = {
-			overallProgress:0,
-			vin:"",
-			maintenance:[]
-		}
-	}
+	// 	this.state = {
+	// 		overallProgress:0,
+	// 		vin:"",
+	// 		maintenance:[]
+	// 	}
+	// }
 
-	captureProgressCategory(progress){
-		// this method doesn't calculate the progress
-		// progress calculation will be done in Tasks component
+	// captureProgressCategory(progress){
+	// 	// this method doesn't calculate the progress
+	// 	// progress calculation will be done in Tasks component
 
-		// this method captures the progress of the category
+	// 	// this method captures the progress of the category
 
-		// this.setState to the new progress of the category
-	}
+	// 	// this.setState to the new progress of the category
+	// }
 
-	addNewCategory(){
-		// this method adds a new category
-		// pass this method down to MainTasks component
-		// this.setState push new category to maintenance
-	}
+	// addNewCategory(){
+	// 	// this method adds a new category
+	// 	// pass this method down to MainTasks component
+	// 	// this.setState push new category to maintenance
+	// }
 
-	addNewTask(){
-		// this method adds a new task
-		// pass this method down to Tasks component
-		// this.setState push new task to maintenance -> category
-	}
+	// addNewTask(){
+	// 	// this method adds a new task
+	// 	// pass this method down to Tasks component
+	// 	// this.setState push new task to maintenance -> category
+	// }
 	
-	deleteCategory(){
-		// this method deletes a category
-		// pass this method down to MainTasks component
-		// this.setState removes the category from maintenance
-	}
+	// deleteCategory(){
+	// 	// this method deletes a category
+	// 	// pass this method down to MainTasks component
+	// 	// this.setState removes the category from maintenance
+	// }
 
-	deleteTask(){
-		// this method deletes a task
-		// pass this method down to Tasks component
-		// this.setState removes the task from maintenance -> category
-	}
+	// deleteTask(){
+	// 	// this method deletes a task
+	// 	// pass this method down to Tasks component
+	// 	// this.setState removes the task from maintenance -> category
+	// }
 
-	componentDidUpdate(prevProps, prevState){
-		// check prevState for all states
+	// componentDidUpdate(prevProps, prevState){
+	// 	// check prevState for all states
 
-		// update overall progress bar
-		// equation: sum of each category's progress / # of categories
-	}
+	// 	// update overall progress bar
+	// 	// equation: sum of each category's progress / # of categories
+	// }
 
 	// Render the component: displays navbar and jumbotron
 	render() {
@@ -77,15 +77,18 @@ export default class Profile extends React.Component {
 		return (
 			<div className="profile-container">
 				<div>
-					{/* Display children components */}
-
-					<Information />
-					<MainTasks />
-					<Tasks />
-
+					test!!!
+					
 				</div>
 
 			</div>
 		);
 	}
 }
+
+/* Display children components 
+
+					<Information />
+					<MainTasks />
+					<Tasks />
+					*/
