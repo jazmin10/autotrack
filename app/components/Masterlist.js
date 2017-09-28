fcda32be1c0606382bd27fe0af12859a8f441afb
Flynn <flynnigan.tan@gmail.com>
2017-09-28 17:13:46 -0500

    updated buttons , took out btn-default

--- app/components/Masterlist.js
+++ app/components/Masterlist.js
@@ -215,7 +215,7 @@ export default class Masterlist extends React.Component {
 
                       <h4 className="masterlist-h4"><strong>{car.year} {car.make} {car.model}</strong></h4>
                       <br/>
-                      <Link to={"/dashboard-manager/profile/" + car.vin + "?token=" + localStorage.getItem("autotrackToken")} className="btn btn-primary masterlist-btn"> View Profile</Link>
+                      <Link to={"/dashboard-manager/profile/" + car.vin + "?token=" + localStorage.getItem("autotrackToken")} className="btn masterlist-btn"> View Profile</Link>
 
                   </div>
 
